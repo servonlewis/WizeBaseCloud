573d26d56c58aa2c701f45fd089afc84a5f323fa
Servon Lewis <servonlewis@gmail.com>
2019-09-29 21:28:17 -0400

    got stuff done

--- src/components/sections/Services1.jsx
+++ src/components/sections/Services1.jsx
@@ -1,71 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import { Button, Icon } from '@material-ui/core';
 
-class Services1 extends Component {
-	state = {};
-
-	serviceList = [
+export default function Services1() {
+	const serviceList = [
 		{
 			icon: 'dashboard',
 			title: 'Business Application',
-			text: 'Lorem ipsum dolor sit amet conse ctetur adipi sicing elit. Doloribus numquam quis.'
+			text: 'Fast, modern, and responsive applications to handle your business needs.'
 		},
 		{
 			icon: 'perm_data_setting',
 			title: 'Custom System Integration',
-			text: 'Lorem ipsum dolor sit amet conse ctetur adipi sicing elit. Doloribus numquam quis.'
+			text: 'Combine all of your Api data into a single cloud data-source.'
 		},
 		{
 			icon: 'storage',
 			title: 'Database Administration',
-			text: 'Lorem ipsum dolor sit amet conse ctetur adipi sicing elit. Doloribus numquam quis.'
+			text: 'Use our Database services to store your company data securely in the cloud.'
 		},
 		{
 			icon: 'stay_primary_portrait',
 			title: 'Custom Mobile Application',
-			text: 'Lorem ipsum dolor sit amet conse ctetur adipi sicing elit. Doloribus numquam quis.'
+			text: 'Take your business to the next level with your very own cross-platform native mobile application.'
 		}
 	];
 
-	render() {
-		return (
-			<section className="section section-service1 light-gray" id="service3">
-				<div className="container">
-					<div className="section__header">
-						<h2>Our Services</h2>
-						<p>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat unde inventore molestias ab
-							adipisci eius nisi placeat at.
-						</p>
-					</div>
-
-					<Grid container spacing={24} alignContent="stretch">
-						{this.serviceList.map((service) => (
-							<Grid item md={3} sm={6} key={service.title}>
-								<Card className="service1__card service__card card">
-									<CardContent className="service1__card__content">
-										<div>
-											<div className="text-center mb-16">
-												<Icon className="card__icon-64">{service.icon}</Icon>
-											</div>
-											<h3 className="font-light">{service.title}</h3>
-											<p>{service.text}</p>
-										</div>
-										<div className="pt-16">
-											<Button>READ MORE</Button>
-										</div>
-									</CardContent>
-								</Card>
-							</Grid>
-						))}
-					</Grid>
+	return (
+		<section className="section section-service1 light-gray" id="service3">
+			<div className="container">
+				<div className="section__header">
+					<h2>Our Services</h2>
+					<p>Wizebase offers all the technologies you need to advertise your company to the world!</p>
 				</div>
-			</section>
-		);
-	}
-}
 
-export default Services1;
+				<Grid container spacing={1} alignContent="stretch">
+					{serviceList.map((service) => (
+						<Grid item md={3} sm={6} key={service.title}>
+							<Card className="service1__card service__card card">
+								<CardContent className="service1__card__content">
+									<div>
+										<div className="text-center mb-16">
+											<Icon className="card__icon-64">{service.icon}</Icon>
+										</div>
+										<h3 className="font-light">{service.title}</h3>
+										<p>{service.text}</p>
+									</div>
+									<div className="pt-16">
+										<Button>READ MORE</Button>
+									</div>
+								</CardContent>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</div>
+		</section>
+	);
+}
